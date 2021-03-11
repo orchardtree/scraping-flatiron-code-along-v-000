6631ae73cf9d272be288db9bc7bae3e1e3feefec
Jesse Wenger <wengerj@gmail.com>
2021-03-11 08:17:25 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -2,6 +2,13 @@ class Course
   @@all = []
   attr_accessor :title, :schedule, :description
   
+  def initialize(title, schedule, description)
+    @title = title 
+    @schedule = schedule
+    @description = description
+    @@all << self
+  end
+  
   def self.all 
     @@all
   end
