5798587ff6c54dbb2bf26ac1c78c209ba2096363
Jesse Wenger <wengerj@gmail.com>
2021-03-11 08:25:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,7 @@ class Scraper
     end
   end
   
-  def self.get_page 
+  def get_page 
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     binding.pry
   end
