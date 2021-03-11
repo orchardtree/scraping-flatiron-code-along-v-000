485010017613d426d9a9dca51dc49ad153e9ea31
Jesse Wenger <wengerj@gmail.com>
2021-03-11 09:01:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -34,10 +34,11 @@ class Scraper
   
   def make courses
     self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
   end
 end
 
