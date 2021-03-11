60a6ed0d56b3cb2e52cb944cb9ccd6dbab322058
Jesse Wenger <wengerj@gmail.com>
2021-03-11 09:01:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,7 +32,7 @@ class Scraper
     self.get_page.css(".post")
   end
   
-  def make courses
+  def make_courses
     self.get_courses.each do |post|
       course = Course.new
       course.title = post.css("h2").text
